020b6501bb9b3bff1f1025d7d20aa2c59b98376d
W. Andrew Loe III <andrew@andrewloe.com>
2010-01-27 22:40:43 -0800

    script/generate scaffold Players

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :players
+
   map.resources :teams
 
   map.root :controller => :teams, :action => :index
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100125013938) do
+ActiveRecord::Schema.define(:version => 20100128063959) do
+
+  create_table "players", :force => true do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "teams", :force => true do |t|
     t.string   "name"
