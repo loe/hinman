e6cf949089b5f7c244318c89a3dbd15f1e3be537
W. Andrew Loe III <andrew@andrewloe.com>
2010-02-22 21:27:55 -0800

    Boat controller tests nested under fleets.

--- test/factories/boat_factory.rb
+++ test/factories/boat_factory.rb
@@ -1,3 +1,4 @@
 Factory.define :boat do |b|
   b.bow '42'
+  b.association :fleet
 end
--- test/functional/boats_controller_test.rb
+++ test/functional/boats_controller_test.rb
@@ -1,45 +1,50 @@
 require 'test_helper'
 
 class BoatsControllerTest < ActionController::TestCase
+  
+  def setup
+    @boat = Factory(:boat)
+  end
+  
   test "should get index" do
-    get :index
+    get :index, :fleet_id => @boat.fleet.to_param
     assert_response :success
     assert_not_nil assigns(:boats)
   end
 
   test "should get new" do
-    get :new
+    get :new, :fleet_id => @boat.fleet.to_param
     assert_response :success
   end
 
   test "should create boat" do
     assert_difference('Boat.count') do
-      post :create, :boat => { }
+      post :create, :fleet_id => @boat.fleet.to_param, :boat => Factory.attributes_for(:boat)
     end
 
-    assert_redirected_to boat_path(assigns(:boat))
+    assert_redirected_to fleet_boat_path(assigns(:fleet), assigns(:boat))
   end
 
   test "should show boat" do
-    get :show, :id => boats(:one).to_param
+    get :show, :fleet_id => @boat.fleet.to_param, :id => @boat.to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => boats(:one).to_param
+    get :edit, :fleet_id => @boat.fleet.to_param, :id => @boat.to_param
     assert_response :success
   end
 
   test "should update boat" do
-    put :update, :id => boats(:one).to_param, :boat => { }
-    assert_redirected_to boat_path(assigns(:boat))
+    put :update, :fleet_id => @boat.fleet.to_param, :id => @boat.to_param, :boat => { }
+    assert_redirected_to fleet_boat_path(assigns(:fleet), assigns(:boat))
   end
 
   test "should destroy boat" do
     assert_difference('Boat.count', -1) do
-      delete :destroy, :id => boats(:one).to_param
+      delete :destroy, :fleet_id => @boat.fleet.to_param, :id => @boat.to_param
     end
 
-    assert_redirected_to boats_path
+    assert_redirected_to fleet_boats_path(assigns(:fleet))
   end
 end
