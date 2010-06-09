3cbafdb8a5e0f71ae568bad86cc3a3c7c62037d7
W. Andrew Loe III <andrew@andrewloe.com>
2010-06-09 00:24:33 -0400

    rails new ./

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '838aba4973b71f752f31ca3fc8e1444b639367088006d68af69c5bc5913a98af259b02d2a4dcc9b24889ab0324e729ce5e0f98e2723266f5caeeb866dc9f52ec'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_hinman_session',
-  :secret      => 'b34600a592dd2a298d554aedbe5f50fb93e40a3fe261891cb4d21e14b4415d3ded18d8058b54adfdfd0399734db0be9c7383340c33138d5b684cdd5d130e4679'
-}
+Rails.application.config.session_store :cookie_store, :key => '_hinman_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Rails.application.config.session_store :active_record_store
