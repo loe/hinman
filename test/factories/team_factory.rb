ed684d2ee0f4274e2fbc68b2594a2ba83b18999c
W. Andrew Loe III <andrew@andrewloe.com>
2010-07-25 23:13:18 -0400

    More test teams.

--- test/factories/team_factory.rb
+++ test/factories/team_factory.rb
@@ -1,3 +1,15 @@
 Factory.define :team do |p|
   p.name 'Sideshow'
 end
+
+Factory.define :ranier do |p|
+  p.name 'Ranier Bears'
+end
+
+Factory.define :panda do |p|
+  p.name 'Silver Panda'
+end
+
+Factory.define :tall do |p|
+  p.name 'Tall Boyz'
+end
