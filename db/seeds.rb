17f98a40961ad967ba147c4506bbc46f7178dd93
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-15 21:44:54 -0700

    Update team seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,150 +1,150 @@
 # coding: utf-8
 {
   
-  "San Diego Yacht Club" => [
-    "Mikee Anderson Mitterling",
-    "David Hughes",
-    "Adam Roberts",
-    "Nick Martin",
-    "Brennan Clark",
-    "Molly O'Bryan"
+  "Big Whoop!" => [
+    {:name => "Brendan Healy", :bio => "Arnold, MD USA"},
+    {:name => "Meghan Jordan", :bio => "East Amhurst, NY USA"},
+    {:name => "Chris Lash", :bio => "Newport, RI USA"},
+    {:name => "Corinna DeCollibus", :bio => "Duxbury, Massachusetts USA"},
+    {:name => "Russ O'Reilly", :bio => "Charleston, SC USA"},
+    {:name => "Joel Labuzetta", :bio => "Annapolis, MD USA"}
   ],
   
-  "Yale Corinthian Yacht Club" => [
-    "Zachary Brown",
-    "Stuart McNay",
-    "Thomas Barrows",
-    "Emmet Smith",
-    "Maria Menninger",
-    "Michael Hession"
+  "Buns & Ammo" => [
+    {:name => "Mark Dinneen", :bio => "Newport, RI USA"},
+    {:name => "Janel Zarkowsky", :bio => "Annapolis, MD USA"},
+    {:name => "Clinton Hayes", :bio => "East Haddam, CT USA"},
+    {:name => "Christine Bletzer", :bio => "Medfield, MA USA"},
+    {:name => "Ben Quatromoni", :bio => "Portsmouth, RI USA"},
+    {:name => "Emily Anderson", :bio => "Northport, NY USA"}
   ],
   
-  "New York Yacht Club" => [
-    "Amanda Callahan",
-    "Clayton Bischoff",
-    "Peter Levesque",
-    "Carrie Amarante",
-    "Caroline Hall Levesque",
-    "Colin Merrick"
-  ],
-  
-  "Hyannis Yacht Club" => [
-    "Matthew Duggan",
-    "Vanessa DeCollibus",
-    "Cardwell Potts",
-    "Alyssa Aitken",
-    "John B. Potts",
-    "Keisha Pearson"
-  ],
-  
-  "Rhode Island Team Racing Association" => [
-    "Joel Hanneman",
-    "Alexa Schuler",
-    "Justin Law",
-    "Lyndsey Gibbons-Neff",
-    "Brian Kamilar",
-    "Megan Riddle"
-  ],
-  
-  "Eastern Yacht Club" => [
-    "Clinton Hayes",
-    "Christine Bletzer",
-    "Mark Dinneen",
-    "Janel Zarkowsky",
-    "Ben Quatromoni",
-    "Emily Anderson"
-  ],
-  
-  "College of Charleston" => [
-    "Brendan Healy",
-    "Meghan Jordan",
-    "Joel Labuzetta",
-    "Corinna Decollibus",
-    "Chris Lash",
-    "Russ O'Reilly"
-  ],
-  
-  "West Kirby Sailing Club" => [
-    "Dom Johnson",
-    "Tom Foster",
-    "Debs Steele",
-    "Andy Cornah",
-    "Hamish Walker",
-    "Ben Field"
-  ],
-  
-  "Eastern Yacht Club/Little Egg Harbor Yacht Club" => [
-    "Clay Johnson",
-    "Elyse Dolbec",
-    "Sean Doyle",
-    "Susan Doyle",
-    "Scott Hogan",
-    "Megan Watson"
-  ],
-  
-  "Royal Bermuda Yacht Club/Newport Harbor Yacht Club" => [
-    "Alexander Kirkland",
-    "Catherine Swanson",
-    "Carly Chamberlain",
-    "Ashley Phillips",
-    "David Siegal",
-    "Jesse Kirkland"
-  ],
-  
-  "Fingerlakes Yacht Club" => [
-    "Trevor Moore",
-    "Mandi Markee",
-    "Brian Clancy",
-    "Chris Klevan",
-    "Lee Sackett",
-    "Oscar Barney"
-  ],
-  
-  "Treasure Island Sailing Center" => [
-    "Kevin Richards",
-    "Avery Patton",
-    "Natasha Baker",
-    "Dan Altreuter",
-    "Linda Stephan",
-    "Patrick Whitmarsh"
-  ],
-  
-  "Newport Harbor Yacht Club" => [
-    "James Silsby",
-    "Garrett Woodworth",
-    "Killarney Loufek",
-    "Stephanie Adams",
-    "Whitney Loufek",
-    "Andy Beeckman"
+  "North Field Alliance" => [
+    {:name => "Alexander Kirkland", :bio => "Warwick, BDA BER"},
+    {:name => "Catherine Swanson", :bio => "Charleston, SC USA"},
+    {:name => "Jesse Kirkland", :bio => "Warwick, BDA BER"},
+    {:name => "Ashley Phillips", :bio => "Charleston, SC USA"},
+    {:name => "David Siegal", :bio => "Newport, RI USA"},
+    {:name => "Carly Chamberlain", :bio => "San Francisco, CA USA"}
   ],
   
   "North Shore Trouble" => [
-    "Benjamin Spiller",
-    "Anna Miniutti",
-    "Matt Allen",
-    "Andrew Schneider",
-    "Charles Higgins",
-    "Maureen Castrucio"
+    {:name => "Benjamin Spiller", :bio => "Somervile, MA USA"},
+    {:name => "Anna Miniutti", :bio => "Jamestown, RI USA"},
+    {:name => "Matt Allen", :bio => "Beverly, MA USA"},
+    {:name => "Andrew Schneider", :bio => "Marblehead, MA USA"},
+    {:name => "Charles Higgins", :bio => "Norfolk, VA USA"},
+    {:name => "Maureen Castrucio", :bio => "Newport, RI USA"}
   ],
   
-  "St. Francis Yacht Club" => [
-    "Harrison Turner",
-    "Jessica Barhydt",
-    "Edward Conrad",
-    "Kristin Maberry",
-    "Holt Condon",
-    "Melanie Roberts"
+  "NYYC Silver Panda" => [
+    {:name => "Colin Merrick", :bio => "Portsmouth, RI USA"},
+    {:name => "Amanda Callahan", :bio => "Canton, MA USA"},
+    {:name => "Clay Bischoff", :bio => "Newport, RI USA"},
+    {:name => "Caroline Hall Levesque", :bio => "Tiverton, RI USA"},
+    {:name => "Peter Levesque", :bio => "Tiverton, RI USA"},
+    {:name => "Carrie Amarante", :bio => "Wayne, NJ USA"}
   ],
   
-  "All-Stars" => [
-    "Ben Ainsle",
-    "Russell Coutts",
-    "Dennis Connor",
-    "Robert Scheidt",
-    "Sir Thomas Lipton",
-    "Paul Elvstrøm"
-  ]
+  "One Man Wolfpack" => [
+    {:name => "James Silsby", :bio => "Huntington Beach, CA USA"},
+    {:name => "Whitney Loufek", :bio => "San Diego, CA USA"},
+    {:name => "Andy Beeckman", :bio => "Huntington Beach, CA USA"},
+    {:name => "Killarney Loufek", :bio => "Mill Valley, CA USA"},
+    {:name => "Garrett Woodworth", :bio => "Venice, CA USA"},
+    {:name => "Stephanie Adams", :bio => "Mountain View, CA USA"}
+  ],
+  
+  "Seattle Sideshow" => [
+    {:name => "Anthony Boscolo", :bio => "Seattle, WA USA"},
+    {:name => "Jen Morgan Glass", :bio => "Seattle, WA USA"},
+    {:name => "Dalton Bergan", :bio => "Seattle, WA USA"},
+    {:name => "Lindsay Bergan", :bio => "Seattle, WA USA"},
+    {:name => "W. Andrew Loe III", :bio => "Seattle, WA USA"},
+    {:name => "Mallory Fontenot", :bio => "Seattle, WA USA"}
+  ],
+  
+  "Statesman with Heron" => [
+    {:name => "Trevor Moore", :bio => "North Pomfret, VT USA"},
+    {:name => "Mandi Markee", :bio => "Del Mar, CA USA"},
+    {:name => "Brian Clancy", :bio => "Annapolis, MD USA"},
+    {:name => "Chris Klevan", :bio => "Norfolk, VA USA"},
+    {:name => "Lee Sackett", :bio => "Cleveland, OH USA"},
+    {:name => "Oscar Barney", :bio => "Portland, OR USA"}
+  ],
+  
+  "Tall Boyz" => [
+    {:name => "Clay Johnson", :bio => "Toms River, NJ USA"},
+    {:name => "Elyse Dolbec", :bio => "Quincy, MA USA"},
+    {:name => "Scott Hogan", :bio => "New York, NY USA"},
+    {:name => "Megan Watson", :bio => "Boston, MA USA"},
+    {:name => "Sean Doyle", :bio => "Cos Cob, CT USA"},
+    {:name => "Susan Doyle", :bio => "Cos Cob, CT USA"}
+  ],
+  
+  "Team Extreme" => [
+    {:name => "Zachary Brown", :bio => "New Haven, CT USA"},
+    {:name => "Emmet Smith", :bio => "New Haven, CT USA"},
+    {:name => "Thomas Barrows", :bio => "Saint Thomas, VI ISV"},
+    {:name => "Maria Menninger", :bio => "New York, NY USA"},
+    {:name => "Stuart McNay", :bio => "Boston, MA USA"},    
+    {:name => "Michael Hession", :bio => "Chicago, IL USA"}
+  ],
+    
+  "Team SDYC" => [
+    {:name => "Mikee Anderson Mitterling", :bio => "Newport Beach, CA USA"},
+    {:name => "David Hughes", :bio => "San Diego, CA USA"},
+    {:name => "Adam Roberts", :bio => "San Diego, CA USA"},
+    {:name => "Brennan Clark", :bio => "San Diego, CA USA"},
+    {:name => "Nicholas Martin", :bio => "San Diego, CA USA"},
+    {:name => "Molly O’Bryan Vandemoer", :bio => "San Diego, CA USA"}
+  ],
+  
+  "Team St.FYC" => [
+    {:name => "Harrison Turner", :bio => "Mountain View, CA USA"},
+    {:name => "Jessica Barhydt", :bio => "San Francisco, CA USA"},
+    {:name => "Holt Condon", :bio => "San Francisco, CA USA"},
+    {:name => "Melanie Roberts", :bio => "San Francisco, CA USA"},
+    {:name => "Edward Conrad", :bio => "San Francisco, CA USA"},
+    {:name => "Kristin Maberry", :bio => "San Francisco, CA USA"}
+  ],
+  
+  "The Woonsocket Rockets" => [
+    {:name => "Joel Hanneman", :bio => "Newport, RI USA"},
+    {:name => "Alexa Schuler", :bio => "Newport, RI USA"},
+    {:name => "Justin Law", :bio => "Newport Beach, CA USA"},
+    {:name => "Megan Riddle", :bio => "Charleston, SC USA"},
+    {:name => "Brian Kamilar", :bio => "Miami, FL USA"},
+    {:name => "Lyndsey Gibbons-Neff", :bio => "Rosemont, PA USA"}
+  ],
+  
+  "Treasure Island Pirates" => [
+    {:name => "Kevin Richards", :bio => "Alameda, CA USA"},
+    {:name => "Avery Patton", :bio => "Oakland, CA USA"},
+    {:name => "Dan Altreuter", :bio => "Redwood City, CA USA"},
+    {:name => "Salley Madsen", :bio => "San Francisco, CA USA"},
+    {:name => "Patrick Whitmarsh", :bio => "Oakland, CA USA"},
+    {:name => "Natasha Baker", :bio => "Oakland, CA USA"}
+  ],
+  
+  "West Kirby Hawks" => [
+    {:name => "Dom Johnson", :bio => "Orpington, Kent GBR"},
+    {:name => "Debs Steele", :bio => "Southampton, Hampshire GBR"},
+    {:name => "Andy Cornah", :bio => "Sevenoaks, Kent GBR"},
+    {:name => "Hamish Walker", :bio => "Southampton, Hampshire GBR"},
+    {:name => "Ben Field", :bio => "Sevenoaks, Kent GBR"},
+    {:name => "Tom Foster", :bio => "Southampton, Hampshire GBR"},
+  ],
 
+  "Who Dat!" => [
+    {:name => "Matthew Duggan", :bio => "Boston, MA USA"},
+    {:name => "Alyssa Aitken", :bio => "Sandwich, MA USA"},
+    {:name => "John Potts", :bio => "New Orleans, LA USA"},
+    {:name => "Keisha Pearson", :bio => "East Patchogue, NY USA"},
+    {:name => "Cardwell Potts", :bio => "Greenwich, CT USA"},
+    {:name => "Vanessa DeCollibus", :bio => "Somerville, MA USA"}
+  ]
+  
 }.each do |team, players|
   t = Team.create(:name => team)
   
