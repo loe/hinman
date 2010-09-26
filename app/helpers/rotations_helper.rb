918e3beea42b69432cf25e5451866805e8c40f65
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-26 01:25:15 -0700

    Setup rotation.

--- app/helpers/rotations_helper.rb
+++ app/helpers/rotations_helper.rb
@@ -1,4 +1,11 @@
 module RotationsHelper
+  
+  def setup_rotation(rotation)
+    rotation.tap do |r|
+      r.teams = [] if r.teams.nil?
+      r.fleets = [] if r.fleets.nil?
+    end
+  end
 
   def team_stats(rotation, team)
     "#{team.win_percentage(rotation)}% (#{team.won_races(rotation).length}/#{team.completed_races(rotation).length})"
