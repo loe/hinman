ba2b23154c3f85dfd3d5c38876d07d64a40294e2
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-26 00:00:06 -0700

    DRY RotationsController, update a few more views to the pretty tables.

--- app/controllers/rotations_controller.rb
+++ app/controllers/rotations_controller.rb
@@ -1,26 +1,22 @@
 class RotationsController < ApplicationController
   
+  before_filter :build_rotation, :only => [:new, :create]
+  before_filter :find_rotation, :only => [:show, :edit, :update, :destroy]
+
   def index
     @rotations = Rotation.all
   end
 
   def show
-    @rotation = Rotation.find(params[:id])
   end
 
   def new
-    @rotation = Rotation.new
-    @teams = Team.all
-    @fleets = Fleet.all
   end
   
   def edit
-    @rotation = Rotation.find(params[:id])
   end
   
   def create
-    @rotation = Rotation.new(params[:rotation])
-
     if @rotation.save
       flash[:notice] = 'Rotation was successfully created.'
       redirect_to(@rotation)
@@ -30,8 +26,6 @@ class RotationsController < ApplicationController
   end
 
   def update
-    @rotation = Rotation.find(params[:id])
-
    if @rotation.update_attributes(params[:rotation])
       flash[:notice] = 'Rotation was successfully updated.'
       redirect_to(@rotation)
@@ -41,8 +35,19 @@ class RotationsController < ApplicationController
   end
 
   def destroy
-    @rotation = Rotation.find(params[:id])
     @rotation.destroy
     redirect_to(rotations_url)
+  end
+  
+  protected
+  
+  def build_rotation
+    @rotation = Rotation.new(params[:rotation])
+    @teams = Team.all
+    @fleets = Fleet.all
+  end
+  
+  def find_rotation
+    @rotation = Rotation.find(params[:id])
   end
 end
