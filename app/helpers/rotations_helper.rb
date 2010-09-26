b89254dbdb5612f25ce5a311aeb45515f8773fe2
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-26 03:01:30 -0700

    Only show completed races for a given rotation.

--- app/helpers/rotations_helper.rb
+++ app/helpers/rotations_helper.rb
@@ -8,6 +8,7 @@ module RotationsHelper
   end
 
   def team_stats(rotation, team)
+    return if team.completed_races(rotation).length == 0
     "#{team.win_percentage(rotation)}% (#{team.won_races(rotation).length}/#{team.completed_races(rotation).length})"
   end
 end
