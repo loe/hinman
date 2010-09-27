58f22a94190bab4513c0e83bfff200e5ecb8e195
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-26 23:14:03 -0700

    Ipmplement the Cyclic Algorithm to generate the Races in a Rotation.

--- app/models/rotation.rb
+++ app/models/rotation.rb
@@ -14,22 +14,25 @@ class Rotation < ActiveRecord::Base
   end
   
   def build_races
-    team_list = teams.present? ? Team.find(teams) : Team.all
+    team_list = (teams.present? ? Team.find(teams) : Team).order('color DESC') # Seed by color which was determined at random.
     fleet_list = fleets.present? ? Fleet.find(fleets) : Fleet.all
     
+    number_of_races_for_a_team = team_list.length - 1
     next_entry = 0
-    while team_list.present? do
-      # Shift off the team.
-      home = team_list.shift
+
+    for i in 1..number_of_races_for_a_team
+      internal_teams = team_list.dup
       
-      # This team races everyone left.
-      team_list.each do |away|
+      while internal_teams.present? do
         race = races.build
-        race.entries.build(:team => home, :fleet => fleet_list.at(next_entry % fleet_list.size))
+        race.entries.build(:team => internal_teams.shift, :fleet => fleet_list.at(next_entry % fleet_list.size))
         next_entry = next_entry + 1
-        race.entries.build(:team => away, :fleet => fleet_list.at(next_entry % fleet_list.size))
+        race.entries.build(:team => internal_teams.shift, :fleet => fleet_list.at(next_entry % fleet_list.size))
         next_entry = next_entry + 1
       end
+
+      team_list.push(team_list.delete_at(1))
     end
+
   end
 end
