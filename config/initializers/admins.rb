3ca4c42a2b72be1449d506e7e948ab8487172441
W. Andrew Loe III <andrew@andrewloe.com>
2010-09-29 22:04:11 -0700

    Add Matt to Admins.

--- config/initializers/admins.rb
+++ config/initializers/admins.rb
@@ -4,5 +4,6 @@ ADMINS = Set.new [
   '607375328', # Dalton Bergan
   '502820844', # Ben Glass
   '1491364622', # Leslie Keller
-  '566447564' # David Ferguson
+  '566447564', # David Ferguson
+  '1627382967' # Matthew Anderson
 ]
