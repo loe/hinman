8bf15d729905c2609502d640cae9fd39f90bb162
W. Andrew Loe III <andrew@andrewloe.com>
2011-07-01 15:52:34 -0700

    Add Bruce to Admins.

--- config/initializers/admins.rb
+++ config/initializers/admins.rb
@@ -1,10 +1,5 @@
 ADMINS = Set.new [
-  '504731' # W. Andrew Loe III
-#  '514550', # Jen Morgan Glass
-#  '607375328', # Dalton Bergan
-#  '502820844', # Ben Glass
-#  '1491364622', # Leslie Keller
-#  '566447564', # David Ferguson
-#  '1627382967', # Matthew Anderson
-#  '1590385247' # Mark Murphy
+  '504731', # W. Andrew Loe III
+  '514550', # Jen Morgan Glass
+  '1428777445' # Bruce Huckaba
 ]
