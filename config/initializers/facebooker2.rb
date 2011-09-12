85a81e17e84914f64766c291d55bf3950b01be92
W. Andrew Loe III <andrew@andrewloe.com>
2011-09-11 19:16:27 -0700

    Handle yaml for development purposes.

--- config/initializers/facebooker2.rb
+++ config/initializers/facebooker2.rb
@@ -1,4 +1,8 @@
-Facebooker2.configuration = {
-  :app_id => ENV['FB_APP_ID'],
-  :secret => ENV['FB_SECRET']
-}
+if Rails.env.development?
+  Facebooker2.load_facebooker_yaml
+else
+  Facebooker2.configuration = {
+    :app_id => ENV['FB_APP_ID'],
+    :secret => ENV['FB_SECRET']
+  }
+end
